9a82d221d4b1c9b6abf5da10eb60672f3ea31480
lrrmarker <laurents.marker@ncas.ac.uk>
2026-01-14 12:39:28 +0000

    updates to scroll bar

--- src/features/MultiUnitScrollBar.tsx
+++ src/features/MultiUnitScrollBar.tsx
@@ -36,7 +36,6 @@ const MultiUnitScrollBar = <T, U>({
   const [content, setContent] = useState<React.ReactNode>([]);
   const [lastKey, setLastKey] = useState<string | null>(null);
   const [keyPress, setKeyPress] = useState<number>(0);
-  console.log(values);
 
   useEffect(() => {
     const newMarks: Mark[] = values.map((val, i) => {
